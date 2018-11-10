d3b67086193e63a0aac172d5145e189f51203598
Janis Böhm <j4nis-boehm@gmail.com>
2018-11-10 14:14:48 +0100

    Initial Commit

---
+++ MetalEngine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalEngine
+//
+//  Created by Janis Böhm on 10.11.18.
+//  Copyright © 2018 Company At The End Of The Universe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
